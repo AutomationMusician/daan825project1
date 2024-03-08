507c36dae7f8ae6e2f34fb8fa636b647e41675b2
joshuafishman <josh.a.fishman@gmail.com>
2024-03-07 21:58:35 -0500

    Change schema for easier reporting

--- get-all-data-for-fact.sql
+++ get-all-data-for-fact.sql
@@ -1,15 +1,10 @@
 SELECT 
-    contribution_fact.amount as amount, contribution_fact.contributor_zip as zip, 
-    contribution_date.date as "date", contribution_date.week_num as week_num,
+    contribution_fact.amount, contribution_fact.num_contributions, contribution_fact.contributor_zip, contribution_fact.contribution_week,
     contributor_employer.name as employer,
     contributor_occupation.name as occupation,
     contributor_state.name as "state",
-    committee.name as committee, committee.party as party,
-    contributor.contributor_name as contributor_name, contributor.contributor_street_1 as contributor_street_1, contributor.contributor_street_2 as contributor_street_2, contributor.contributor_city as contributor_city
+    committee.name as committee, committee.party as party
 FROM contribution_fact
-
-JOIN contribution_date
-ON contribution_fact.contribution_date_id=contribution_date.id
 
 JOIN contributor_employer
 ON contribution_fact.contributor_employer_id=contributor_employer.id
@@ -23,7 +18,6 @@ ON contribution_fact.contributor_state_id=contributor_state.id
 JOIN committee
 ON contribution_fact.committee_id=committee.id
 
-JOIN contributor
-ON contribution_fact.contributor_id = contributor.id
+ORDER BY contribution_fact.num_contributions DESC
 
-WHERE contribution_fact.id = 1;
+LIMIT 1;
--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -6,16 +6,6 @@ CREATE DATABASE warehouse;
 
 \c warehouse;
 
--- contribution_date
-CREATE TABLE contribution_date (
-    id serial,
-    "date" DATE UNIQUE,
-    week_num NUMERIC(1),
-    CONSTRAINT contribution_date_pk PRIMARY KEY (id)
-);
-CREATE INDEX contribution_date_idx ON contribution_date("date");
-CREATE INDEX contribution_date_week_num_idx ON contribution_date(week_num);
-
 -- contributor_employer
 CREATE TABLE contributor_employer (
     id serial,
@@ -50,33 +40,18 @@ CREATE TABLE committee (
 CREATE INDEX committee_name_idx ON committee("name");
 CREATE INDEX committee_party_idx ON committee(party);
 
--- contributor
-CREATE TABLE contributor (
-    id serial,
-    contributor_name VARCHAR(150),
-    contributor_street_1 VARCHAR(50),
-    contributor_street_2 VARCHAR(50),
-    contributor_city VARCHAR(50),
-    contributor_state CHAR(2),
-    contributor_zip INT,
-    CONSTRAINT contributor_pk PRIMARY KEY (id)
-);
-
 -- fact table
 CREATE TABLE contribution_fact (
     id serial,
-    contribution_date_id INT,
+    contribution_week INT,
     contributor_state_id INT,
     contributor_zip INT,
     contributor_employer_id INT,
     contributor_occupation_id INT,
     committee_id INT,
-    contributor_id INT,
     amount NUMERIC(8,2),
+    num_contributions INT,
     CONSTRAINT contribution_fact_pk PRIMARY KEY(id),
-    CONSTRAINT contribution_fact_contribution_date_fk
-        FOREIGN KEY(contribution_date_id)
-            REFERENCES contribution_date(id),
     CONSTRAINT contribution_fact_contributor_state_fk
         FOREIGN KEY(contributor_state_id)
             REFERENCES contributor_state(id),
@@ -88,15 +63,11 @@ CREATE TABLE contribution_fact (
             REFERENCES contributor_occupation(id),
     CONSTRAINT contribution_fact_committee_fk
         FOREIGN KEY(committee_id)
-            REFERENCES committee(id),
-    CONSTRAINT contribution_fact_contributor_fk
-        FOREIGN KEY(contributor_id)
-            REFERENCES contributor(id)
+            REFERENCES committee(id)
 );
 CREATE INDEX contribution_fact_committee_id_idx ON contribution_fact(committee_id);
-CREATE INDEX contribution_fact_contributor_id_idx ON contribution_fact(contributor_id);
 CREATE INDEX contribution_fact_contributor_state_id_idx ON contribution_fact(contributor_state_id);
 CREATE INDEX contribution_fact_contributor_zip_idx ON contribution_fact(contributor_zip);
 CREATE INDEX contribution_fact_contributor_employer_id_idx ON contribution_fact(contributor_employer_id);
 CREATE INDEX contribution_fact_contributor_occupation_id_idx ON contribution_fact(contributor_occupation_id);
-CREATE INDEX contribution_fact_contribution_date_id_idx ON contribution_fact(contribution_date_id);
+CREATE INDEX contribution_fact_contribution_week_idx ON contribution_fact(contribution_week);
